51de58217e5068c31200739ef17a04a8d8b399fb
William <williamlaitpe@gmail.com>
2024-07-02 21:24:03 -0400

    update Restaurant and SearchResult entity

--- src/entity/Restaurant.java
+++ src/entity/Restaurant.java
@@ -1,4 +1,95 @@
 package entity;
+import java.util.Arrays;
 
 public class Restaurant {
-}
+    // Attributes of the restaurant
+    private String name;
+    private String location;
+    private int accumulatedReviewers;
+    private float averageRating;
+    private String[] dishTypes;
+    private String menu;
+    private String summarizedReview;
+
+    // Constructor
+    public Restaurant(String name, String location, int accumulatedReviewers, float averageRating, String[] dishTypes, String menu, String summarizedReview) {
+        this.name = name;
+        this.location = location;
+        this.accumulatedReviewers = accumulatedReviewers;
+        this.averageRating = averageRating;
+        this.dishTypes = dishTypes;
+        this.menu = menu;
+        this.summarizedReview = summarizedReview;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getAccumulatedReviewers() {
+        return accumulatedReviewers;
+    }
+
+    public float getAverageRating() {
+        return averageRating;
+    }
+
+    public String[] getDishTypes() {
+        return dishTypes;
+    }
+
+    public String getMenu() {
+        return menu;
+    }
+
+    public String getSummarizedReview() {
+        return summarizedReview;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setAccumulatedReviewers(int accumulatedReviewers) {
+        this.accumulatedReviewers = accumulatedReviewers;
+    }
+
+    public void setAverageRating(float averageRating) {
+        this.averageRating = averageRating;
+    }
+
+    public void setDishTypes(String[] dishTypes) {
+        this.dishTypes = dishTypes;
+    }
+
+    public void setMenu(String menu) {
+        this.menu = menu;
+    }
+
+    public void setSummarizedReview(String summarizedReview) {
+        this.summarizedReview = summarizedReview;
+    }
+
+    // toString method for easy printing
+    @Override
+    public String toString() {
+        return "Restaurant{" +
+                "name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                ", accumulatedReviewers=" + accumulatedReviewers +
+                ", averageRating=" + averageRating +
+                ", dishTypes=" + Arrays.toString(dishTypes) +
+                ", menu='" + menu + '\'' +
+                ", summarizedReview='" + summarizedReview + '\'' +
+                '}';
+    }
--- src/entity/SearchResult.java
+++ src/entity/SearchResult.java
@@ -1,4 +1,29 @@
 package entity;
+import java.util.Arrays;
 
 public class SearchResult {
-}
+    // Array to store the list of restaurants
+    private Restaurant[] restaurantList;
+
+    // Constructor
+    public SearchResult(Restaurant[] restaurantList) {
+        this.restaurantList = restaurantList;
+    }
+
+    // Getter for restaurantList
+    public Restaurant[] getRestaurantList() {
+        return restaurantList;
+    }
+
+    // Setter for restaurantList
+    public void setRestaurantList(Restaurant[] restaurantList) {
+        this.restaurantList = restaurantList;
+    }
+
+    // toString method for easy printing
+    @Override
+    public String toString() {
+        return "SearchResult{" +
+                "restaurantList=" + Arrays.toString(restaurantList) +
+                '}';
+    }
