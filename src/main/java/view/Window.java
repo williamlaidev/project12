a675fd09cc235f3a70f529d1ec8c2b58216b049c
whyimhere <145921688+whyimhere@users.noreply.github.com>
2024-07-04 19:49:47 -0400

    add search bar to the window

--- src/main/java/view/Window.java
+++ src/main/java/view/Window.java
@@ -4,9 +4,17 @@ import javax.swing.*;
 public class Window {
     public static void main(String[] args) {
         JFrame f=new JFrame("Window Example");
-        JButton b=new JButton("Click Here");
-        b.setBounds(50,100,95,30);
+        JButton b=new JButton("Search");
+        b.setBounds(50,300,95,30);
         f.add(b);
+
+        JTextField t1,t2;
+        t1=new JTextField("Enter your distance here");
+        t1.setBounds(50,100, 200,30);
+        t2=new JTextField("Enter your dish type here");
+        t2.setBounds(50,150, 200,30);
+        f.add(t1); f.add(t2);
+
         f.setSize(900,600);
         f.setLayout(null);
         f.setVisible(true);
