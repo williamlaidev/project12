1f31ede1ddd4823631a1dd623affc7592102e1b9
whyimhere <145921688+whyimhere@users.noreply.github.com>
2024-07-11 18:51:07 -0400

    Adjust elements' position

--- src/vision/CalculatePosition.java
+++ src/vision/CalculatePosition.java
@@ -3,9 +3,34 @@ package vision;
 
 public class CalculatePosition {
 
-    public static int[] Search(int windowWidth, int windowHeight)  {
-        int x = windowWidth / 2;
-        int y = windowHeight / 2;
+
+    public static int[] SearchButton(int windowWidth, int windowHeight)  {
+        int x = windowWidth / 5 + 500;
+        int y = windowHeight / 5;
+        int width = 100;
+        int height = 30;
+        int[] result = new int[4];
+        result[0] = x;
+        result[1] = y;
+        result[2] = width;
+        result[3] = height;
+        return result;
+    }
+    public static int[] SearchBar(int windowWidth, int windowHeight)  {
+        int x = windowWidth / 5;
+        int y = windowHeight / 5;
+        int width = 500;
+        int height = 30;
+        int[] result = new int[4];
+        result[0] = x;
+        result[1] = y;
+        result[2] = width;
+        result[3] = height;
+        return result;
+    }
+    public static int[] SearchHint(int windowWidth, int windowHeight) {
+        int x = windowWidth / 5;
+        int y = windowHeight / 5;
         int width = 100;
         int height = 30;
         int[] result = new int[4];
--- src/vision/StartWindow.java
+++ src/vision/StartWindow.java
@@ -6,26 +6,31 @@ public class StartWindow {
 
     public static void main(String[] args) {
 
-        int width = 1920;
-        int height = 1080;
+        int width = 960;
+        int height = 600;
 
 
         JFrame startMenu=new JFrame("Start Window");
 
         final JTextField searchField=new JTextField();
-        searchField.setBounds(50,50, 150,20);
+        int[] searchBarPosition = CalculatePosition.SearchBar(width, height);
+        searchField.setBounds(searchBarPosition[0], searchBarPosition[1], searchBarPosition[2], searchBarPosition[3]);
 
         JButton searchButton=new JButton("Search");
-        int[] searchPosition = CalculatePosition.Search(width, height);
-        searchButton.setBounds(searchPosition[0],searchPosition[1],searchPosition[2],searchPosition[3]);
+        int[] searchButtonPosition = CalculatePosition.SearchButton(width, height);
+        searchButton.setBounds(searchButtonPosition[0],searchButtonPosition[1],
+                searchButtonPosition[2],searchButtonPosition[3]);
         searchButton.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 searchField.setText("Welcome to Javatpoint.");
             }
         });
 
+
         JLabel searchHint = new JLabel("Add your desired dish here!");
-        searchHint.setBounds(100,200, 500,30);
+        int[] searchHintPosition = CalculatePosition.SearchHint(width, height);
+        searchHint.setBounds(searchHintPosition[0], searchHintPosition[1],
+                searchHintPosition[2], searchHintPosition[3]);
 
         startMenu.add(searchButton);
         startMenu.add(searchField);
