ac977d6f8a4e37600250a45282cc99b89f44b981
brynn <jbh5684@gmail.com>
2024-07-17 18:33:12 -0400

    Adding DAO folder

---
+++ src/main/java/app/dao/SearchQueryDAO.java
@@ -0,0 +1,12 @@
+package main.java.app.dao;
+
+import main.java.entity.SearchQuery;
+import java.util.List;
+
+public interface SearchQueryDao {
+    void save(SearchQuery searchQuery);
+    SearchQuery findById(int searchQueryId);
+    List<SearchQuery> findAll();
+    void update(SearchQuery searchQuery);
+    void delete(int searchQueryId);
+}
