35d80664133a8981352e894ae3a2076f56e69f6b
brynn <jbh5684@gmail.com>
2024-07-17 18:13:23 -0400

    Adding dao folder

---
+++ src/main/java/app/dao/ReviewDAO.java
@@ -0,0 +1,12 @@
+package app.dao;
+
+import main.java.entity.Review;
+import java.util.List;
+
+public interface ReviewDao {
+    void save(Review review);
+    Review findById(int reviewId);
+    List<Review> findAll();
+    void update(Review review);
+    void delete(int reviewId);
+}
--- src/main/java/app/dao/impl/SearchQueryDaoImpl.java
+++ src/main/java/app/dao/impl/SearchQueryDaoImpl.java
@@ -1,7 +1,36 @@
-package app.dao.impl;
-
 import app.dao.SearchQueryDao;
 import main.java.entity.SearchQuery;
 
 import java.sql.Connection;
-import java.sql
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchQueryDaoImpl implements SearchQueryDao {
+    private Connection connection;
+
+    public SearchQueryDaoImpl(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public void save(SearchQuery searchQuery) {
+        // SQL to save search query
+    }
+
+    @Override
+    public SearchQuery findById(int searchQueryId) {
+        // SQL to find search query by ID
+        return null;
+    }
+
+    @Override
+    public List<SearchQuery> findAllByUserId(int userId) {
+        // SQL to find all search queries by user ID
+        return new ArrayList<>();
+    }
+
+    @Override
+    public void delete(int searchQueryId) {
+        // SQL to delete search query
+    }
+}
