24f9dc5ff7e738c8d15e792e4dc6ef5d12dd69d9
William <williamlaitpe@gmail.com>
2024-07-20 16:53:49 -0400

    fix(json): fix deleted files

--- src/main/java/use_case/SearchRestaurantsByDistance.java
+++
@@ -1,9 +0,0 @@
-package use_case;
-
-import entity.Location;
-import entity.Restaurant;
-import java.util.List;
-
-public interface SearchRestaurantsByDistance {
-    List<Restaurant> search(Location location, int radius, int maxResults);
-}
