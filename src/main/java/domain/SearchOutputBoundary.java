d18092c7e0de7ace4e0908d5bb0ed739e158e449
kerakim <keraakim@gmail.com>
2024-07-28 19:32:25 -0400

    add javadoc to SearchOutputBoundary

--- src/main/java/domain/SearchOutputBoundary.java
+++ src/main/java/domain/SearchOutputBoundary.java
@@ -1,7 +1,15 @@
 package domain;
 
-// TODO: Add JavaDoc documentation to describe the purpose and usage.
-
+/**
+ * Interface that defines what to do when a search operation fails.
+ * This interface is used to show error messages when a search doesn't work.
+ */
 public interface SearchOutputBoundary {
+
+    /**
+     * Prepares the failure view with the given error message.
+     *
+     * @param error the error message to be presented
+     */
     void prepareFailView(String error);
 }
