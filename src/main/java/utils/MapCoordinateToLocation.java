a95b2e35784d501ba574f38eba78c841cbe600eb
kerakim <keraakim@gmail.com>
2024-07-28 19:23:40 -0400

    add javadoc

--- src/main/java/utils/MapCoordinateToLocation.java
+++ src/main/java/utils/MapCoordinateToLocation.java
@@ -2,9 +2,24 @@ package utils;
 
 import java.awt.*;
 
-// TODO: Add JavaDoc documentation to describe the purpose and usage.
+/**
+ * Utility class for converting map coordinates to geographical locations.
+ * This class provides a method to convert a point on a map to latitude and longitude values.
+ */
 
 public class MapCoordinateToLocation {
+
+    /**
+     * Converts a point on a map to geographical coordinates (latitude and longitude).
+     *
+     * @param point the point on the map to be converted
+     * @param centerLat the latitude of the map's center
+     * @param centerLng the longitude of the map's center
+     * @param zoomLevel the zoom level of the map
+     * @param mapWidth the width of the map in pixels
+     * @param mapHeight the height of the map in pixels
+     * @return an array containing the latitude and longitude of the converted point
+     */
     public static double[] convert(Point point, double centerLat, double centerLng, int zoomLevel, int mapWidth, int mapHeight) {
         double metersPerPixel = ZoomLevelToMeterPerPixel.zoomLevelToMeter(zoomLevel, centerLat);
 
